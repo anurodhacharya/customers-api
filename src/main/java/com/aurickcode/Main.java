7dd2b61cd5a1c9e7f12e031eb5779ae130d5072c
anurodhacharya <anurodh.acharya10@gmail.com>
2023-12-05 00:26:57 -0800

    new things

--- src/main/java/com/aurickcode/Main.java
+++ src/main/java/com/aurickcode/Main.java
@@ -15,7 +15,7 @@ import com.aurickcode.customer.CustomerRepository;
 public class Main { 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }   
+    }    
 
     @Bean
     CommandLineRunner runner(CustomerRepository customerRepository) {
