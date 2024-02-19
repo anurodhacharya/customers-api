d28187dabef4eeac410427844c65b101e9b3ab39
anurodhacharya <anurodh.acharya10@gmail.com>
2024-02-18 19:42:58 -0800

    New Changes

--- backend/src/main/java/com/aurickcode/exception/RequestValidationException.java
+++ backend/src/main/java/com/aurickcode/exception/RequestValidationException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class RequestValidationException extends RuntimeException{
     public RequestValidationException(String message) {
-        super(message);
+        super(message); 
     }
 }
